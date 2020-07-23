494205c6c6b74de21941340045c44e8ee879704b
Jesus Pernia <jesuse.epg@gmail.com>
2020-07-23 17:41:41 -0400

    React router integration and base setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Login from './views/Login';
+import NotFound from './views/NotFound';
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
+      <Switch>
+        <Route path="/" component={Login} exact />
+        <Route component={NotFound} />
+      </Switch>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +26,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
